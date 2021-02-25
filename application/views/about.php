048bd9debea7702e33f62c8de4385e6ccc7616d1
= <developer@cluebixsoftware.com>
2021-02-25 16:39:58 +0530

    About page content updated

--- application/views/about.php
+++ application/views/about.php
@@ -28,5 +28,97 @@
         </div>
     </section>
 
-   <section></section>
+    <section class="services misVis mt-5 mb-5">
+       <img src="<?=base_url()?>assets/patterns/r2.png" alt="p" class="pattern1">
+       <img src="<?=base_url()?>assets/patterns/r1.png" alt="p" class="pattern3">
+       <div class="container mt-5 mb-5">
+           <div class="row justify-content-center flex-wrap-reverse">
+               <div class="col-md-5 ml-md-0 ml-5 no-head mr-0 mr-sm-0 mt-md-0 mt-5">
+                   <div class="service pl-5 pr-3 py-5 text-white">
+                        <span>
+                            <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="90" height="90" viewBox="0 0 140.104 140.105">
+                                <defs>
+                                    <clipPath id="clip-path">
+                                    <circle id="Oval" cx="70.052" cy="70.052" r="70.052" fill="#0071d8"/>
+                                    </clipPath>
+                                </defs>
+                                <g id="Circle">
+                                    <circle id="Oval-2" data-name="Oval" cx="70.052" cy="70.052" r="70.052" fill="#0071d8"/>
+                                    <g id="Circle-2" data-name="Circle" clip-path="url(#clip-path)">
+                                    <g id="Color_Secondary" data-name="Color/Secondary">
+                                        <rect id="Rectangle" width="140.105" height="140.105" fill="#0071d8"/>
+                                    </g>
+                                    </g>
+                                </g>
+                                <g id="mission" transform="translate(26.475 22.175)">
+                                    <path id="Path_153" data-name="Path 153" d="M69.932,62.773A36.884,36.884,0,0,0,55.775,38.436v-6.6A42.5,42.5,0,0,0,43.246,1.588l-.141-.141a1.526,1.526,0,0,0-2.16,0l-.141.141A42.5,42.5,0,0,0,28.277,31.835v6.6A36.884,36.884,0,0,0,14.12,62.773l-1.106,8.3a1.529,1.529,0,0,0,1.025,1.65,1.559,1.559,0,0,0,.489.079,1.527,1.527,0,0,0,1.326-.77l1.563-2.736a14.294,14.294,0,0,1,10.86-7.087v1.427A1.528,1.528,0,0,0,29.8,65.163h1.528v4.583a1.528,1.528,0,0,0,1.528,1.528H40.5v6.111h3.055V71.273h7.638a1.528,1.528,0,0,0,1.528-1.528V65.163h1.528a1.528,1.528,0,0,0,1.528-1.528V62.21A14.29,14.29,0,0,1,66.635,69.3L68.2,72.033a1.528,1.528,0,0,0,2.84-.962ZM42.026,4.72a39.6,39.6,0,0,1,7.531,11.557H34.494A39.6,39.6,0,0,1,42.026,4.72ZM16.934,64.782l.214-1.606A33.806,33.806,0,0,1,28.277,42.414V59.133A17.385,17.385,0,0,0,16.934,64.782Zm32.73,3.436H34.387V65.163H49.664Zm3.055-6.111H31.332V31.835a39.8,39.8,0,0,1,2.006-12.5H50.715a39.84,39.84,0,0,1,2,12.5Zm3.055-2.973V42.414A33.822,33.822,0,0,1,66.9,63.177l.214,1.606A17.377,17.377,0,0,0,55.775,59.134Z" fill="#fff"/>
+                                    <path id="Path_154" data-name="Path 154" d="M35.166,19a9.166,9.166,0,1,0,9.166,9.166A9.175,9.175,0,0,0,35.166,19Zm0,15.277a6.111,6.111,0,1,1,6.111-6.111A6.117,6.117,0,0,1,35.166,34.277Z" transform="translate(6.86 9.498)" fill="#fff"/>
+                                    <path id="Path_155" data-name="Path 155" d="M43.075,48.445l-2.542,1.694,1.624,2.437a7,7,0,0,1-.874,8.827l-5.59,5.59L30.1,61.4a7,7,0,0,1-.874-8.827l1.624-2.437-2.542-1.694-1.624,2.437a10.049,10.049,0,0,0,1.256,12.681l6.67,6.67a1.527,1.527,0,0,0,2.16,0l6.67-6.67A10.049,10.049,0,0,0,44.7,50.882Z" transform="translate(6.332 25.036)" fill="#fff"/>
+                                </g>
+                            </svg>
+                           <h3 class="lato ml-4 font-weight-bold">Mission</h3>
+                           <small class="d-block ml-4 mb-3 mt-3">
+                                To consistently deliver transformative Digital Marketing & Web Solutions and provide the best customer results possible with a WOW factor through our services.
+                           </small>
+                        </span>
+                    </div>
+               </div>
+               <div class="col-md-5 ml-5 mt--10 with-head">
+                   <a class="text-right d-block orange-highlight">&nbsp;</a>
+                   <h2 class="text-right mb-5 lato font-weight-bold">What are our <br> <span class="lato orange-highlight">thoughts</span></h2>
+                   <div class="service pr-sm-5 pl-sm-3 py-5 pl-5 pr-3 text-white">
+                        <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="90" height="90" viewBox="0 0 140.105 140.105">
+                            <defs>
+                                <clipPath id="clip-path">
+                                <circle id="Oval" cx="70.052" cy="70.052" r="70.052" fill="#0071d8"/>
+                                </clipPath>
+                            </defs>
+                            <g id="Circle">
+                                <circle id="Oval-2" data-name="Oval" cx="70.052" cy="70.052" r="70.052" fill="#0071d8"/>
+                                <g id="Circle-2" data-name="Circle" clip-path="url(#clip-path)">
+                                <g id="Color_Secondary" data-name="Color/Secondary">
+                                    <rect id="Rectangle" width="140.105" height="140.105" fill="#0071d8"/>
+                                </g>
+                                </g>
+                            </g>
+                            <g id="eye" transform="translate(31.342 1.776)">
+                                <path id="Path_153" data-name="Path 153" d="M152.673,134.013a18.66,18.66,0,1,0,18.66,18.66A18.681,18.681,0,0,0,152.673,134.013Zm15.464,16.594c-1.837.493-5.091,1.042-6.689-.638-.395-.415-.733-.846-1.061-1.263-.923-1.173-1.968-2.5-4-2.808a5.786,5.786,0,0,0-4.077,1.143c-.423.262-.949.587-1.141.539-4.063-1.053-3.769-4.314-3.015-8.15a15.955,15.955,0,0,0,.288-1.776,15.608,15.608,0,0,1,19.7,12.951Zm-23.023-11.578a15.515,15.515,0,0,0-.3,5.991,7.024,7.024,0,0,0,5.584,5.524,4.37,4.37,0,0,0,3.52-.9,3,3,0,0,1,2.012-.718c.8.12,1.2.6,2.049,1.673.354.45.755.96,1.249,1.479a7.437,7.437,0,0,0,5.573,2.075,15.621,15.621,0,0,0,3.434-.418,15.484,15.484,0,0,1-1.39,5.452c-1.02-.772-2.26-1.689-2.916-2.092a7.384,7.384,0,0,0-6.173-1.145c-2.97.928-4.616,4-5.6,6.234a10.73,10.73,0,0,1-5.193,5l-.007,0a15.588,15.588,0,0,1-3.223-1.735l.294-.167c1.814-1.029,3.87-2.2,3.84-4.48a5.177,5.177,0,0,0-1.571-3.064,8.332,8.332,0,0,1-1.756-2.86,15.4,15.4,0,0,1-.507-3.924,9.221,9.221,0,0,0-3.972-7.47A15.721,15.721,0,0,1,145.114,139.029Zm-8.041,13.644a15.508,15.508,0,0,1,1.435-6.534,6.174,6.174,0,0,1,2.473,4.86,18.441,18.441,0,0,0,.62,4.709,10.889,10.889,0,0,0,2.335,3.975,7.242,7.242,0,0,1,.876,1.2c-.064.48-1.4,1.237-2.289,1.743-.405.23-.818.464-1.221.715A15.543,15.543,0,0,1,137.072,152.673Zm15.6,15.6q-.793,0-1.566-.078a12.124,12.124,0,0,0,3.856-4.782c.739-1.684,1.928-3.988,3.708-4.544a4.526,4.526,0,0,1,3.658.831c.567.349,1.858,1.306,2.958,2.142A15.59,15.59,0,0,1,152.673,168.273Z" transform="translate(-113.514 -84.807)" fill="#fff"/>
+                                <path id="Path_154" data-name="Path 154" d="M78.036,115.012a77.161,77.161,0,0,0-11.263-12.154,58.175,58.175,0,0,0-12.288-8.39,34.549,34.549,0,0,0-15.325-3.873,34.551,34.551,0,0,0-15.325,3.873,58.175,58.175,0,0,0-12.288,8.39A77.172,77.172,0,0,0,.281,115.012a1.53,1.53,0,0,0,0,1.769,77.167,77.167,0,0,0,11.263,12.154,58.175,58.175,0,0,0,12.288,8.39A34.549,34.549,0,0,0,39.158,141.2a34.551,34.551,0,0,0,15.325-3.873,58.175,58.175,0,0,0,12.288-8.39A77.167,77.167,0,0,0,78.035,116.78,1.529,1.529,0,0,0,78.036,115.012ZM64.761,126.629c-6.016,5.25-15.341,11.509-25.6,11.509-10.24,0-19.547-6.234-25.551-11.464A77.916,77.916,0,0,1,3.439,115.9a77.725,77.725,0,0,1,10.117-10.733c6.016-5.25,15.341-11.509,25.6-11.509,10.24,0,19.547,6.234,25.551,11.464A77.909,77.909,0,0,1,74.878,115.9,77.746,77.746,0,0,1,64.761,126.629Z" transform="translate(0 -48.03)" fill="#fff"/>
+                                <path id="Path_155" data-name="Path 155" d="M247.53,39.293a1.53,1.53,0,0,0,1.53-1.53V35.421a1.53,1.53,0,0,0-3.059,0v2.343A1.53,1.53,0,0,0,247.53,39.293Z" transform="translate(-208.371)" fill="#fff"/>
+                                <path id="Path_156" data-name="Path 156" d="M96.381,86.721a1.53,1.53,0,0,0,2.163-2.163L96.888,82.9a1.53,1.53,0,0,0-2.163,2.163Z" transform="translate(-79.856 -41.134)" fill="#fff"/>
+                                <path id="Path_157" data-name="Path 157" d="M388.423,87.168a1.525,1.525,0,0,0,1.082-.448l1.657-1.657A1.53,1.53,0,0,0,389,82.9l-1.657,1.657a1.53,1.53,0,0,0,1.082,2.611Z" transform="translate(-327.713 -41.133)" fill="#fff"/>
+                                <path id="Path_158" data-name="Path 158" d="M247.53,442.794a1.53,1.53,0,0,0-1.53,1.53v2.343a1.53,1.53,0,1,0,3.059,0v-2.343A1.53,1.53,0,0,0,247.53,442.794Z" transform="translate(-208.371 -346.356)" fill="#fff"/>
+                                <path id="Path_159" data-name="Path 159" d="M389.5,399.167a1.53,1.53,0,0,0-2.163,2.163L389,402.987a1.53,1.53,0,0,0,2.163-2.163Z" transform="translate(-327.712 -309.022)" fill="#fff"/>
+                                <path id="Path_160" data-name="Path 160" d="M96.382,399.166l-1.657,1.657a1.53,1.53,0,0,0,2.163,2.163l1.657-1.657a1.53,1.53,0,1,0-2.163-2.163Z" transform="translate(-79.856 -309.022)" fill="#fff"/>
+                            </g>
+                        </svg>
+                        <h3 class="lato ml-4 font-weight-bold">Vision</h3>
+                        <small class="d-block ml-4 mb-3 mt-3">
+                            Our vision is to create a business world full of prosperity, meaning & delivering measurable results to clients, employees, and vendors.
+                        </small>
+                   </div>
+               </div>
+           </div>
+       </div>
+   </section>
+
+   <section class="huge mt-5 bg-blue about-huge">
+       <div class="container pt-3">
+           <div class="row flex-wrap-reverse align-items-center">
+               <div class="col-md-7 mt-5 mt-md-0 text-white">
+                   <h3 class="mb-3 font-weight-bold">
+                    Looking to take your business online or improving your <span class="orange-highlight">online </span> <span class="orange-highlight">presence?</span>
+                   </h3>
+                   <p class="mb-4">Connect with us, share your idea & we can help you take your business online easily and in no time. </p>
+                   <a href="<?=base_url('contact#contact')?>" class="btn js-tilt btn-outline-light ">
+                       <span class="d-inline-flex">
+                           Connect now <i class="material-icons ml-2">arrow_right_alt</i>
+                    </span></a>
+               </div>
+               <div class="col-md-5 text-center align-self-center">
+                    <img src="<?=base_url()?>assets/thumb.png" class="thumb" width="220" alt="thumb">  
+               </div>
+           </div>
+       </div>
+   </section>
     
